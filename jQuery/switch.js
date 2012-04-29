98b04c37ae40d9227b803eb8cc941ed70ad51e83
Shea <shea.ryan.177@my.csun.edu>
2012-04-29 13:39:29 -0700

    Working version without any extras

--- jQuery/switch.js
+++ jQuery/switch.js
@@ -18,12 +18,51 @@ $(function(){
 
 
 //jsonp("user_likeiness", "userID=Shea", function(data){
-	//console.log(data);
+//console.log(data);
 //});
 
 
 
-jsonp("thing_likeiness", "userID=Shea", function(data){
+/*jsonp("thing_likeiness", "userID=Shea", function(data){
 	data = ["gyu", "some other place"];
 	console.log(data);
-});
+});*/
+
+var addPlace = function(){
+	var data = $("#formAdd").children("input:not([type=submit]), select");
+	var reviewObject = {attr:{}};
+	data.each(function(){
+		if (this.tagName === "SELECT")
+			reviewObject.attr[this.name]=this.value;
+		else
+			reviewObject[this.name]=this.value;
+	});
+
+	var jsonData = JSON.stringify(reviewObject);
+	jsonp("rate", "data=" +  encodeURIComponent(jsonData), function(data){
+		console.log(data);
+		$("#addResult").show();
+	});
+};
+
+var findPlace = function(){
+	var data = $("#formFind input[name=userID]");
+	/*May add attributes later, but only userID for now*/
+	jsonp("thing_likeiness", "userID="+encodeURIComponent(data.val()), function(data){
+		console.log(data);
+		$('#findResults').show();
+		var obj = data;
+		for(var i=0;i<obj.length;i++){
+			var attrhtml="";
+			for(attr in obj[i]){
+				if(attr==="placeName") continue;
+				attrhtml+= attr+": "+obj[i][attr]+"<br>";
+			}
+			$('#findResults').html("<div style='text-align:left;margin:20px'><h3>"+obj[i].placeName+"</h3>"+attrhtml+"</div>");
+		}
+	});
+
+	//send query to API
+	//print out results to div
+};
+
--- main.php
+++ main.php
@@ -4,19 +4,26 @@ include_once("./header.html");
 
 <div id="find">
 <h1>Find Places</h1>
-<form action="#######FILL THIS IN" method="post" name="findForm">
-<label>Your Username: </label><input type="text" name="uname" value="Alex" /><br>
+<div class="findy">
+<form action="javascript:findPlace()" name="findForm" id="formFind">
+<label>Your Username: </label><input type="text" name="userID" value="Alex" /><br>
 <label>Your Location (ZIP): </label><input type="text" name="location" value="90024" /><br>
 <input class="submitButton" type = "submit" value = "Submit" />
 </form>
+</div>
+<div id="findResults" class="findy">
+<h2>Results</h2>
+</div>
+<div style="clear:both"></div>
 </div>
 
 
 <div id="add">
 <h1>Review Places</h1>
-<form action="########SUBMISSION FILL THIS IN" method="post" name="addForm">
-<label>Your Username: </label> <input type="text" name="uname" value="Alex" /><br>
-<label>Place Name: </label><input type="text" name="thing" /><br>
+<div class="addy">
+<form action="javascript:addPlace()" name="addForm" id="formAdd">
+<label>Your Username: </label> <input type="text" name="userID" value="Alex" /><br>
+<label>Place Name: </label><input type="text" name="thingID" /><br>
 <!--Attribute:Rating pairs-->
 <label>Food: </label><select name="food">
 <?php
@@ -49,6 +56,9 @@ for($i = 1; $i <= 5; $i++){
 </select><br>
 <input class="submitButton" type="submit" value = "Submit Review" />
 </form>
+</div>
+<div class="addy" id ="addResult"><h2>Submitted OK</h2></div>
+<div style="clear:both"></div>
 </div>
 </body>
 </html>
