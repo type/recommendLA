6d2cf2ed052a6b1e30b3361b59e550c5bc5bf93e
Shea <shea.ryan.177@my.csun.edu>
2012-04-28 21:08:13 -0700

    Some front end

---
+++ main.php
@@ -0,0 +1,18 @@
+<?php
+include_once("./header.html");
+?>
+
+<div id="find">
+<h1 id="appTitle">Find Places That Aren't Awful</h1>
+<?php
+//Add the "Find" form in here
+?>
+</div>
+
+
+<div id="add">
+<h1 id = "addTitle">Review Places</h1>
+<?php
+//Add the "Add" form in here
+?>
+</div>
